44a45dd79b237751a2e8d2e549d2e14801a9ae90
Dairon M. Leyva <daironmichel@gmail.com>
2019-10-12 14:59:25 -0400

    viewer schema

--- api/graphql/types.py
+++ api/graphql/types.py
@@ -4,12 +4,30 @@ from graphene_django.types import DjangoObjectType
 from trader.models import RiskManagementSettings
 
 
+class DatabaseId(graphene.Interface):
+    database_id = graphene.Int()
+
+    def resolve_database_id(self, info, **kwargs):
+        return getattr(self, 'id')
+
+
 class RiskManagementSettingsNode(DjangoObjectType):
     class Meta:
         exclude_fields = ('user',)
         model = RiskManagementSettings
-        interfaces = (relay.Node, )
+        interfaces = (relay.Node, DatabaseId)
+
+
+class Viewer(graphene.ObjectType):
+    risk_management_settings = graphene.Field(RiskManagementSettingsNode)
+
+    def resolve_risk_management_settings(self, info, **kwargs):
+        return info.context.user.risk_management_settings
 
 
 class Query(graphene.ObjectType):
+    viewer = graphene.Field(Viewer)
     risk_management_settings = relay.Node.Field(RiskManagementSettingsNode)
+
+    def resolve_viewer(self, info, **kwargs):
+        return Viewer()
--- trader/models.py
+++ trader/models.py
@@ -9,7 +9,8 @@ class RiskManagementSettings(models.Model):
     profit = models.IntegerField()
     loss = models.IntegerField()
     fee_per_trade = models.FloatField()
-    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True)
+    user = models.OneToOneField(
+        User, on_delete=models.CASCADE, null=True, related_name='risk_management_settings')
 
     class Meta:
         verbose_name = 'Risk Managent Settings'
