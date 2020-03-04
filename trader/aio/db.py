5c430539e96b52d92b6241cd21a7cc76290b3d65
Dairon Leyva <dleyva@peopleticker.com>
2020-03-04 14:46:06 -0500

    close connections on async to sync decorator

--- trader/aio/db.py
+++ trader/aio/db.py
@@ -1,5 +1,5 @@
 from asgiref.sync import SyncToAsync
-from django.db import close_old_connections
+from django.db import close_old_connections, connections
 
 
 class DatabaseSyncToAsync(SyncToAsync):
@@ -8,11 +8,13 @@ class DatabaseSyncToAsync(SyncToAsync):
     """
 
     def thread_handler(self, loop, *args, **kwargs):
-        close_old_connections()
+        # close_old_connections()
+        connections.close_all()
         try:
             return super().thread_handler(loop, *args, **kwargs)
         finally:
-            close_old_connections()
+            connections.close_all()
+            # close_old_connections()
 
 
 # The class is TitleCased, but we want to encourage use as a callable/decorator
